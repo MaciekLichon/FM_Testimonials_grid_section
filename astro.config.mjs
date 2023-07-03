83787ecc59759b5d7610383623f2a1816310b430
maciejlichon <maciej.lichon@geckodynamics.com>
2023-07-03 09:52:02 +0200

    publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://MaciekLichon.github.io',
+    base: '/FM_Testimonials_grid_section'
+});
